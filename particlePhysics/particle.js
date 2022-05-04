dbb30e07d915a065a491b5f5cd8f265ad3042e73
danielx-art <metacarpo10@gmail.com>
2022-05-04 00:49:19 -0300

    display function particle 1

--- particlePhysics/particle.js
+++ particlePhysics/particle.js
@@ -164,36 +164,47 @@ const createParticle = function(
         }
     }
 
-    let displayDependencies = display.dependencies.map((item) => {
-        //item can be stuff like display.scale so we have to split of '.' and nest eah one like particle['display']['scale']
-        particle[item]
-    })
+    self.display = function(p5inst){
 
-    self['display'] = (p5inst, dirVector) => {
-        p5inst.noStroke();
-        p5inst.fill(0);
-        //p5inst.ellipse(self.pos.x, self.pos.y, 15, 15);
-        if(dirVector){
-            p5inst.stroke(0);
-            p5inst.line(self.pos.x, self.pos.y, self.pos.x + self.dir.x*100, self.pos.y + self.dir.y*100);
-            p5inst.stroke(255,0,0);
-            p5inst.line(self.pos.x, self.pos.y, self.pos.x + self.magnet.m.x/10, self.pos.y + self.magnet.m.y/10);
-        }
-        p5inst.beginShape();
-        let rH = 20;
-        let rW = 10;
-        let tempH = vec().copy(self.dir).setMag(rH);
-        let tempW = vec().copy(tempH).rotate(Math.PI/2).setMag(rW);
-        let vertex1 = vec().copy(self.pos).add(tempH).add(tempW);
-        p5inst.vertex(vertex1.x, vertex1.y);
-        let vertex2 = vec().copy(self.pos).add(tempH).add(tempW.rotate(Math.PI));
-        p5inst.vertex(vertex2.x, vertex2.y);
-        let vertex3 = vec().copy(self.pos).add(tempH.rotate(Math.PI)).add(tempW);
-        p5inst.vertex(vertex3.x, vertex3.y);
-        let vertex4 = vec().copy(self.pos).add(tempH).add(tempW.rotate(Math.PI));
-        p5inst.vertex(vertex4.x, vertex4.y);
-        p5inst.endShape();
+        let displayDependencies = display.dependencies.map((item) => 
+        {
+            let thisDependency = item.split(".");
+            let lastDependency = thisDependency.pop();
+            let context = self;
+            for (let i = 0; i < thisDependency.length; i++) {
+                context = context[thisDependency[i]];
+            }
+            return context[displayLast];
+        });
+
+        return display.displayFunction(p5inst, ...displayDependencies)
     }
+
+    // self['display'] = (p5inst, dirVector) => {
+    //     p5inst.noStroke();
+    //     p5inst.fill(0);
+    //     //p5inst.ellipse(self.pos.x, self.pos.y, 15, 15);
+    //     if(dirVector){
+    //         p5inst.stroke(0);
+    //         p5inst.line(self.pos.x, self.pos.y, self.pos.x + self.dir.x*100, self.pos.y + self.dir.y*100);
+    //         p5inst.stroke(255,0,0);
+    //         p5inst.line(self.pos.x, self.pos.y, self.pos.x + self.magnet.m.x/10, self.pos.y + self.magnet.m.y/10);
+    //     }
+    //     p5inst.beginShape();
+    //     let rH = 20;
+    //     let rW = 10;
+    //     let tempH = vec().copy(self.dir).setMag(rH);
+    //     let tempW = vec().copy(tempH).rotate(Math.PI/2).setMag(rW);
+    //     let vertex1 = vec().copy(self.pos).add(tempH).add(tempW);
+    //     p5inst.vertex(vertex1.x, vertex1.y);
+    //     let vertex2 = vec().copy(self.pos).add(tempH).add(tempW.rotate(Math.PI));
+    //     p5inst.vertex(vertex2.x, vertex2.y);
+    //     let vertex3 = vec().copy(self.pos).add(tempH.rotate(Math.PI)).add(tempW);
+    //     p5inst.vertex(vertex3.x, vertex3.y);
+    //     let vertex4 = vec().copy(self.pos).add(tempH).add(tempW.rotate(Math.PI));
+    //     p5inst.vertex(vertex4.x, vertex4.y);
+    //     p5inst.endShape();
+    // }
 
     return self;
 };
